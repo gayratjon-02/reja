ace69dc65f23e2e42e61058bea5d79dbebd51a14
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-06-06 01:22:23 +0900

    feat: completed  A-MITASK

--- train.js
+++ train.js
@@ -1,3 +1,53 @@
+// A-TASK: 
+
+
+function countLetter (letter,  word){
+   const letters = word.split("");
+   const found = letters.filter((ll) => ll === letter);
+   console.log(`${word} sozida ${found.length} ta ${letter} harfi bor `);
+    
+    
+      
+};
+
+
+
+
+
+countLetter("e",  "engeenering");
+
+
+
+// TASK A
+
+// Harf sifatida kiritilgan birinchi parametr, 
+// kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+// Funktsiya tuzing
+
+// Masalan: countLetter("e", "engineer")
+// 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+// // 3 sonini qaytaradi
+
+
+// Assalomu alaykum
+
+// MITASK’larni train.js fileda yozasiz!
+
+// yozish tartibingiz:
+
+// Masalani izohi
+// A-TASK: 
+
+// Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+
+// masalani yechimi:
+// Qilgan Yechimingiz…
+
+
+// agarda npm run train deganimda train.js ishga tushmasa yengilgina “FAIL” bo’lasiz!
+
+
 //  *********************************************************
 // Video 1
 
@@ -51,30 +101,30 @@
 
 
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling", // 0-20
-    "Togri boshliq tanlang va koproq hato qiling", // 20-30
-    "Uzingizga ishlatishni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", // 40-50
-    "yoshlarga inversitsiya qiling", // 50-60
-    "endi dam oling foydasi yoq" , //60
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba boling", // 0-20
+//     "Togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "Uzingizga ishlatishni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga inversitsiya qiling", // 50-60
+//     "endi dam oling foydasi yoq" , //60
 
-];
+// ];
 
-async function maslahatBering(a){
-    if(typeof a !== "number") throw new Error ("Please insert the Number");
-    else if(a <= 20) return list[0];
-    else if(a > 20 && a <= 30 ) return list[1];
-    else if(a > 30 && a <= 40 ) return list[1];
-    else if(a > 40 && a <= 50 ) return list[1];
-    else if(a > 50 && a <= 60 ) return list[1];
-    else{
-        return new Promise((resolve, reject) => {
-            setInterval(() => {
-                resolve(list[5]);
-            }, 1000);
-        });
+// async function maslahatBering(a){
+//     if(typeof a !== "number") throw new Error ("Please insert the Number");
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30 ) return list[1];
+//     else if(a > 30 && a <= 40 ) return list[1];
+//     else if(a > 40 && a <= 50 ) return list[1];
+//     else if(a > 50 && a <= 60 ) return list[1];
+//     else{
+//         return new Promise((resolve, reject) => {
+//             setInterval(() => {
+//                 resolve(list[5]);
+//             }, 1000);
+//         });
 
 
         // return new Promise((resolve, reject) => {
@@ -86,9 +136,9 @@ async function maslahatBering(a){
 
         // setTimeout(() => {
         //     callback(null, list[5]);
-        // }, 5000);
-    }
-};
+//         // }, 5000);
+//     }
+// };
 
 // console.log("Passead here");
 
@@ -105,17 +155,17 @@ async function maslahatBering(a){
 
 //
 
-async function run() {
-    let javob = await maslahatBering(19);
-    console.log(javob);
-    javob = await maslahatBering(33);
-    console.log(javob);
-    javob = await maslahatBering(66);
-    console.log(javob);
-    javob = await maslahatBering(53);
-    console.log(javob);
-    javob = await maslahatBering(66);
-    console.log(javob);
-}
+// async function run() {
+//     let javob = await maslahatBering(19);
+//     console.log(javob);
+//     javob = await maslahatBering(33);
+//     console.log(javob);
+//     javob = await maslahatBering(66);
+//     console.log(javob);
+//     javob = await maslahatBering(53);
+//     console.log(javob);
+//     javob = await maslahatBering(66);
+//     console.log(javob);
+// }
 
-run();
+// run();
