04be70b8e219e5af1abe482f44d450d238e9959a
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-06-08 01:36:30 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,73 @@
+console.log('Web Serverni boshlash');
+const express = require('express');
+const app = express();
+// express() orqali app obectini chaqirib olamiz
+const fs = require("fs");
+
+
+// MongoDB chaqirish
+
+const db = require("./server").db();
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err){
+        console.log("ERROR:", err);
+    }else{
+        user = JSON.parse(data);
+    }
+});
+
+// 1 : Kirish code
+app.use(express.static("public"));
+// Har qanday browserdan kirib kelayotgan sorovlar
+// uchun public folder ochiq degan manoni anglatadi.
+// keinchalik style va image'larni shu folderga joylashtiriladi
+
+app.use(express.json());
+// bu kodimiz  kirib kelayotgan json formatdagi
+// datani object'ga ogirib beradi
+
+app.use(express.urlencoded({extended: true}));
+// HTML form'dan POST qilinganda express server 
+// qabul qilib olish uchun yoziladi. bu yozilmasa
+// formdan express qabul qilmaydi
+
+// 2 SESSION =========
+
+// 3 Views code: Traditional usulda ya'ni backend'da view yasab
+// uni cliend(user)'ga yuboramiz....
+app.set("views", "views");
+// view folder korsatiladi
+app.set("view engine", "ejs")
+// view egine'ni ejs ekanligi korsatiladi
+// Ejs orqali backend ichida frontend(view) yasaymiz
+
+// 4 Routiing code 
+
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
+    
+})
+
+
+app.get("/", function(req, res){
+    res.render("reja");
+});
+// app.get("/hello", function(req, res){
+//     res.end(`<h1 style = "background: red">HELLO WORLD by Gayratjon(ALI)</h1>`);
+// });
+
+// app.get("/gift", function(req, res){
+//     res.end('siz sovgalar bolimidasiz')
+// })
+
+
+
+ module.exports = app;
--- server.js
+++ server.js
@@ -1,76 +1,30 @@
-console.log('Web Serverni boshlash');
-const express = require('express');
-const app = express();
-// express() orqali app obectini chaqirib olamiz
+
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb")
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err){
-        console.log("ERROR:", err);
-    }else{
-        user = JSON.parse(data);
+let db;
+const connectionString = "mongodb+srv://gayratjon02:MoreThan02@cluster0.fssdmk5.mongodb.net/Reja";
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true,},
+    (err, client) => {
+        if(err) console.log("ERROR on connection MongoDB");
+        else{
+            console.log("MongoDB connection succeed")
+            module.exports = client;
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function(){
+                console.log((`The server is running seccessfully on port: ${PORT}, http://localhost:${PORT}`));
+            });
+
+        }
     }
-});
-
-// 1 : Kirish code
-app.use(express.static("public"));
-// Har qanday browserdan kirib kelayotgan sorovlar
-// uchun public folder ochiq degan manoni anglatadi.
-// keinchalik style va image'larni shu folderga joylashtiriladi
-
-app.use(express.json());
-// bu kodimiz  kirib kelayotgan json formatdagi
-// datani object'ga ogirib beradi
-
-app.use(express.urlencoded({extended: true}));
-// HTML form'dan POST qilinganda express server 
-// qabul qilib olish uchun yoziladi. bu yozilmasa
-// formdan express qabul qilmaydi
-
-// 2 SESSION =========
-
-// 3 Views code: Traditional usulda ya'ni backend'da view yasab
-// uni cliend(user)'ga yuboramiz....
-app.set("views", "views");
-// view folder korsatiladi
-app.set("view engine", "ejs")
-// view egine'ni ejs ekanligi korsatiladi
-// Ejs orqali backend ichida frontend(view) yasaymiz
-
-// 4 Routiing code 
-
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-})
-
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
-    
-})
-
-
-app.get("/", function(req, res){
-    res.render("reja");
-});
-// app.get("/hello", function(req, res){
-//     res.end(`<h1 style = "background: red">HELLO WORLD by Gayratjon(ALI)</h1>`);
-// });
-
-// app.get("/gift", function(req, res){
-//     res.end('siz sovgalar bolimidasiz')
-// })
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log((`The server is running seccessfully on port: ${PORT}, http://localhost:${PORT}`));
-});
+);
 
 
 
 
- 
+
+
+
